0536f4ffa83ea270e000e89216ac05069451b6fd
shirleygordon <gshirleyg@gmail.com>
2022-07-03 21:19:23 +0300

    Added clearing of board every time it's displayed and onSquareClick function

--- View/js/display-board.js
+++ View/js/display-board.js
@@ -1,8 +1,15 @@
+import '../../common/common.js';
+import { EMPTY_NUMBER } from '../../common/common.js';
+import { trySwitchPlaces } from '../../controller/number-board/game.js';
+
 const squareTypes = {
     "number": getNumberSquare
 }
 
 export function displayBoard(board) {
+    let boardElement = document.getElementById("board");
+    boardElement.innerHTML = "";
+    
     board.forEach(line => {
         let lineElement = document.createElement("div");
         lineElement.classList.add("line");
@@ -10,12 +17,14 @@ export function displayBoard(board) {
         line.forEach(square => {
             let squareElement = document.createElement("div");
             squareElement.classList.add("square");
+            squareElement.setAttribute("id", "square");
+            squareElement.onclick = function() { onSquareClick(board, square) };
             let content = squareTypes[typeof square](square);
             squareElement.appendChild(content);
             lineElement.appendChild(squareElement);
         })
         
-        document.getElementById("board").appendChild(lineElement);
+        boardElement.appendChild(lineElement);
     });
 }
 
@@ -24,9 +33,18 @@ function getNumberSquare(square) {
     let content = document.createElement("p");
     content.classList.add("number-content");
 
-    if (square != EMPTY) {
+    if (square != EMPTY_NUMBER) {
         content.appendChild(document.createTextNode(square));
     }
 
     return content;
+}
+
+function onSquareClick(board, square) {
+    try {
+        let newBoard = trySwitchPlaces(board, square);
+        displayBoard(newBoard);
+    } catch(error) {
+        alert(error);
+    }
 }
